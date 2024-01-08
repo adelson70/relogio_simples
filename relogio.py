3fd0b3497c527324f8ccfefc8fc798da7b5ece13
adelson70 <129080230+adelson70@users.noreply.github.com>
2024-01-08 18:36:02 -0300

    ajustando funções

--- relogio.py
+++ relogio.py
@@ -3,6 +3,8 @@
 import tkinter as tk
 from tkinter.font import Font
 from datetime import datetime
+import requests
+from tkinter import messagebox
 
 # Função para ajustar a janela principal conforme o conteudo que estiver nela
 def ajustar_janela_ao_conteudo(root):
@@ -21,11 +23,29 @@ def ajustar_janela_ao_conteudo(root):
     # Define a geometria da janela
     root.geometry(f"{largura+200}x{altura}+{x_pos-50}+{y_pos-120}")
 
+def obter_horario_brasilia():
+    try:
+        # Especifica o fuso horário para Brasília
+        response = requests.get('http://worldtimeapi.org/api/timezone/America/Sao_Paulo')
+        dados = response.json()
+        horario_brasilia = dados['datetime']
+
+        objeto_datetime = datetime.strptime(horario_brasilia,"%Y-%m-%dT%H:%M:%S.%f%z")
+
+        return objeto_datetime
+    
+    except:
+        return messagebox.showerror('Erro','Erro ao consultar o horário de Brasília, Verifique sua Internet!')
+
 def buscar_horario():
-    ...
+    horario_completo = obter_horario_brasilia()
 
 def buscar_data():
-    ...
+    horario_completo = obter_horario_brasilia()
+
+    dia = str(horario_completo.day)
+    mes = str(horario_completo.month)
+    ano = str(horario_completo.year)
 
 
 # Janela Principal do Relógio
